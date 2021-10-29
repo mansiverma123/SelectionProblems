b6c7f282d2c59f32028464b4cf03e72ebce855ee
mansiverma123 <vmansi144@gmail.com>
2021-10-30 04:03:51 +0530

    Added:Calculated Unit conversion

--- Selection.sh
+++ Selection.sh
@@ -1,12 +1,8 @@
 #!/bin/bash -x
 
-for i in {1..5}
-do
-Num=$(( ( RANDOM% 99 )+100 ))
-Sum=$(($Sum+$Num))
-Avg=$(($Sum/5))
-done
+inch=1/12;
 
-echo Sum = $(($Sum))
-echo Avg = $(($Avg))
+NumToConvert=42
+UnitConv=$(($NumToConvert*$inch))
+echo 42 inches = $UnitConv feets
 
